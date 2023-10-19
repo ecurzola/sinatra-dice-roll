f65b66e4ae737f3032082d86093489f7c69031b2
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-10-19 05:59:59 +0000

    last changes

--- dice.rb
+++ dice.rb
@@ -7,12 +7,15 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<h1>Dice Roll</h1>
-  <p>Roll two 6-sided dice</p>
-  <p>Roll two 10-sided dice</p>
-  <p>Roll two 20-sided dice</p>
-  <p>Roll five 4-sided dice</p>
-  " 
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
