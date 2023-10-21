63e6a893d5c49cd1787181deff7f8d43d2a112c5
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-10-21 19:39:06 +0000

    Changed to 1d20

--- dice.rb
+++ dice.rb
@@ -48,14 +48,12 @@ get("/dice/2/10") do
    <p>#{outcome}</p>"
 end
 
-get("/dice/2/20") do
+get("/dice/1/20") do
   first_die = rand(1..20)
-  second_die = rand(1..20)
-  sum = first_die + second_die
+  
+  outcome = "You rolled a #{first_die}."
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d20</h1>
+  "<h1>1d20</h1>
    <p>#{outcome}</p>"
 end
 
